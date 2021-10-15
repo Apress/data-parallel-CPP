f0f268d0e801aeaf25292346cf432fb685f77e00
Alexander Batashev <alexander.batashev@intel.com>
2021-10-15 14:20:21 +0300

    Update samples to use SYCL 2020 APIs

--- samples/Ch10_expressing_kernels/fig_10_7_opencl_source_interop.cpp
+++ samples/Ch10_expressing_kernels/fig_10_7_opencl_source_interop.cpp
@@ -2,7 +2,9 @@
 
 // SPDX-License-Identifier: MIT
 
+#include <CL/cl.h>
 #include <CL/sycl.hpp>
+#include <CL/sycl/backend/opencl.hpp>
 #include <array>
 #include <iostream>
 using namespace sycl;
@@ -22,14 +24,28 @@ int main() {
     // Note: This must select a device that supports interop!
     queue Q{ cpu_selector{} };
 
-    program p{Q.get_context()};
-    p.build_with_source(R"CLC(
+    cl_context OCLContext = get_native<backend::opencl>(Q.get_context());
+    cl_device_id OCLDeviceID = get_native<backend::opencl>(Q.get_device());
+
+    const std::string Source = R"CLC(
             kernel void add(global int* data) {
                 int index = get_global_id(0);
                 data[index] = data[index] + 1;
             }
-        )CLC",
-        "-cl-fast-relaxed-math");
+        )CLC";
+
+    const char *SourceStr = Source.c_str();
+    const size_t SourceSize = Source.size();
+
+    cl_int Ret;
+    cl_program OCLProgram =
+        clCreateProgramWithSource(OCLContext, 1, &SourceStr, &SourceSize, &Ret);
+    Ret = clBuildProgram(OCLProgram, 1, &OCLDeviceID, "-cl-fast-relaxed-math",
+                         nullptr, nullptr);
+    cl_kernel OCLKernel = clCreateKernel(OCLProgram, "add", &Ret);
+
+    kernel SYCLKernel =
+        make_kernel<backend::opencl>(OCLKernel, Q.get_context());
 
     std::cout << "Running on device: "
               << Q.get_device().get_info<info::device::name>() << "\n";
@@ -38,9 +54,13 @@ int main() {
       accessor data_acc {data_buf, h};
 
       h.set_args(data_acc);
-      h.parallel_for(size, p.get_kernel("add"));
+      h.parallel_for(size, SYCLKernel);
     });
-// END CODE SNIP
+
+    clReleaseKernel(OCLKernel);
+    clReleaseProgram(OCLProgram);
+    clReleaseContext(OCLContext);
+    // END CODE SNIP
   }
 
   for (int i = 0; i < size; i++) {
