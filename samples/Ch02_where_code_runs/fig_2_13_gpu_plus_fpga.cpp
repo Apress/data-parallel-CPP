0a383d0721c05fbaff0432b6d5297d386af1cd58
Michael Kinsner <michael.kinsner@intel.com>
2023-03-16 16:17:51 -0300

    Use new fpga_selector_v to match SYCL 2020 selector form.  Available in oneapi 2023.1.

--- samples/Ch02_where_code_runs/fig_2_13_gpu_plus_fpga.cpp
+++ samples/Ch02_where_code_runs/fig_2_13_gpu_plus_fpga.cpp
@@ -3,13 +3,13 @@
 // SPDX-License-Identifier: MIT
 
 #include <sycl/sycl.hpp>
-#include <sycl/ext/intel/fpga_extensions.hpp> // For fpga_selector
+#include <sycl/ext/intel/fpga_extensions.hpp> // For fpga_selector_v
 #include <iostream>
 using namespace sycl;
 
 int main() {
   queue my_gpu_queue( gpu_selector_v );
-  queue my_fpga_queue( ext::intel::fpga_selector{} );
+  queue my_fpga_queue( ext::intel::fpga_selector_v );
 
   std::cout << "Selected device 1: " <<
     my_gpu_queue.get_device().get_info<info::device::name>() << "\n";
