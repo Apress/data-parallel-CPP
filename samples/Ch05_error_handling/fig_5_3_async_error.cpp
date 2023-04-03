9a60920fb4e8319985fce4d206dd811a188b0c13
Michael Kinsner <michael.kinsner@intel.com>
2023-04-03 13:00:35 -0300

    New and portable mechanism to trigger example asynchonous error

--- samples/Ch05_error_handling/fig_5_3_async_error.cpp
+++ samples/Ch05_error_handling/fig_5_3_async_error.cpp
@@ -5,13 +5,12 @@
 #include <sycl/sycl.hpp>
 using namespace sycl;
 
-// Our simple asynchronous handler function
+// Our example asynchronous handler function
 auto handle_async_error = [](exception_list elist) {
   for (auto &e : elist) {
     try { std::rethrow_exception(e); }
-    catch ( sycl::exception& e ) {
-      std::cout << "ASYNC EXCEPTION!!\n";
-      std::cout << e.what() << "\n";
+    catch ( ... ) {
+      std::cout << "Caught SYCL ASYNC exception!!\n";
     }
   }
 };
@@ -21,18 +20,18 @@ void say_device(const queue& Q) {
     << Q.get_device().get_info<info::device::name>() << "\n";
 }
 
-int main() { 
-  queue Q1{ gpu_selector_v, handle_async_error };
-  queue Q2{ cpu_selector_v, handle_async_error };
-  say_device(Q1);
-  say_device(Q2);
+class something_went_wrong{};  // Example exception type
 
-  try {
-    Q1.submit([&] (handler &h){
-        // Empty command group is illegal and generates an error
-        },
-        Q2); // Secondary/backup queue!
-  } catch (...) {}  // Discard regular C++ exceptions for this example
+int main() { 
+  queue Q{ cpu_selector_v, handle_async_error };
+  say_device(Q);
+
+  Q.submit([&] (handler &h){
+    h.host_task( [](){
+      throw( something_went_wrong{} );
+    });
+  }).wait_and_throw();
+
   return 0;
 }
 
