ff6c48e0894b0f94287010d066f8339d6b29514c
Michael Kinsner <michael.kinsner@intel.com>
2023-04-03 14:09:58 -0300

    Update to modern/portable async exception creation, and catch non-sycl exception in default handler example

--- samples/Ch05_error_handling/fig_5_9_default_handler_proxy.cpp
+++ samples/Ch05_error_handling/fig_5_9_default_handler_proxy.cpp
@@ -13,6 +13,8 @@ auto handle_async_error = [](exception_list elist) {
     try { std::rethrow_exception(e); }
     catch ( sycl::exception& e ) {
       // Print information about the asynchronous exception
+    } catch (...) {
+      // Print information about non-sycl::exception
     }
   }
 
@@ -28,18 +30,18 @@ void say_device(const queue& Q) {
     << Q.get_device().get_info<info::device::name>() << "\n";
 }
 
-int main() { 
-  queue Q1{ gpu_selector_v, handle_async_error };
-  queue Q2{ cpu_selector_v, handle_async_error };
-  say_device(Q1);
-  say_device(Q2);
+class something_went_wrong{};
 
-  try {
-    Q1.submit([&] (handler &h){
-        // Empty command group is illegal and generates an error
-        },
-        Q2); // Secondary/backup queue!
-  } catch (...) {}  // Discard regular C++ exceptions for this example
+int main() { 
+  queue Q{ cpu_selector_v, handle_async_error };
+  say_device(Q);
+
+  Q.submit([&] (handler &h){
+    h.host_task( [](){
+      throw( something_went_wrong{} );
+      });
+    }).wait();
+
   return 0;
 }
 
