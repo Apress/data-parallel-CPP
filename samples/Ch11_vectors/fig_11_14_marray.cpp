4be92db2c865765560dbf2c69b7ced2b4c2d9c29
xtian <xinmin.tian@intel.com>
2023-05-03 13:10:10 -0700

    Adjusted problem size for marray test (#62)

--- samples/Ch11_vectors/fig_11_14_marray.cpp
+++ samples/Ch11_vectors/fig_11_14_marray.cpp
@@ -4,6 +4,7 @@
 
 #include <cassert>
 #include <sycl/sycl.hpp>
+#define M 32
 
 using namespace sycl;
 
@@ -20,22 +21,25 @@ bool checkEqual(marray<T, N> A, marray<T, N> B) {
 int main() {
   queue q;
   marray<float, 4> input{1.0004f, 1e-4f, 1.4f, 14.0f};
-  marray<float, 4> res[2] = {{-1, -1, -1, -1}, {-2, -2, -2, -2}};
+  marray<float, 4> res[M];
+  for (int i=0; i < M; i++)
+    res[i] = {-(i+1), -(i+1), -(i+1), -(i+1)};
   {
     buffer in_buf(&input, range{1});
-    buffer re_buf(res, range{2});
+    buffer re_buf(res, range{M});
 
     q.submit([&](handler &cgh) {
       accessor re_acc{re_buf, cgh, read_write};
       accessor in_acc{in_buf, cgh, read_only};
 
-      cgh.parallel_for(range<1>(2), [=](id<1> idx) {
+      cgh.parallel_for(range<1>(M), [=](id<1> idx) {
         int i = idx[0];
         re_acc[i] = cos(in_acc[0]);
       });
     });
   }
-  if (checkEqual(res[0], res[1]))
+
+  if (checkEqual(res[0], res[M/2]))
     std::cout << "passed\n";
   else 
     std::cout << "failed\n";
