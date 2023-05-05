f5e3b6a4ca4d64824c88047a055535ac1c36e28d
James Reinders <github@collect3.com>
2023-05-04 18:45:37 -0700

    updated Fig 12 7 for 2nd edition (#72)
    * updated Fig 12-7 for second edition
    
    * updated for 2nd edition
    
    * updated for 2nd edition
    
    ---------
    
    Co-authored-by: u177099 <u177099@s001-n157.aidevcloud>

--- samples/Ch12_device_information/fig_12_7_very_curious.cpp
+++ samples/Ch12_device_information/fig_12_7_very_curious.cpp
@@ -33,12 +33,73 @@ int main() {
       std::cout << "  Device: "
                 << dev.get_info<info::device::name>()
                 << "\n";
+      // is_cpu() == has(aspect::cpu)
       std::cout << "    is_cpu(): "
                 << (dev.is_cpu() ? "Yes" : "No") << "\n";
+      // is_cpu() == has(aspect::gpu)
       std::cout << "    is_gpu(): "
                 << (dev.is_gpu() ? "Yes" : "No") << "\n";
+      // is_cpu() == has(aspect::accelerator)
       std::cout << "    is_accelerator(): "
                 << (dev.is_accelerator() ? "Yes" : "No")
+                << "\n";
+
+#if 0
+      std::cout << "    has(emulated): "
+                << (dev.has(aspect::emulated) ? "Yes" : "No") << "\n";
+#endif
+      std::cout << "    has(fp16): "
+                << (dev.has(aspect::fp16) ? "Yes" : "No")
+                << "\n";
+      std::cout << "    has(fp64): "
+                << (dev.has(aspect::fp64) ? "Yes" : "No")
+                << "\n";
+#if 0
+      std::cout << "    has(host_debuggable): "
+                << (dev.has(aspect::host_debuggable) ? "Yes"
+                                                     : "No")
+                << "\n";
+#endif
+      std::cout << "    has(atomic64): "
+                << (dev.has(aspect::atomic64) ? "Yes"
+                                              : "No")
+                << "\n";
+      // see Chapter 13
+      std::cout << "    has(queue_profiling): "
+                << (dev.has(aspect::queue_profiling) ? "Yes"
+                                                     : "No")
+                << "\n";
+      std::cout << "    has(usm_device_allocations): "
+                << (dev.has(aspect::usm_device_allocations)
+                        ? "Yes"
+                        : "No")
+                << "\n";
+      std::cout << "    has(usm_host_allocations): "
+                << (dev.has(aspect::usm_host_allocations)
+                        ? "Yes"
+                        : "No")
+                << "\n";
+      std::cout << "    has(usm_atomic_host_allocations): "
+                << (dev.has(
+                        aspect::usm_atomic_host_allocations)
+                        ? "Yes"
+                        : "No")
+                << "\n";
+      std::cout << "    has(usm_shared_allocations): "
+                << (dev.has(aspect::usm_shared_allocations)
+                        ? "Yes"
+                        : "No")
+                << "\n";
+      std::cout
+          << "    has(usm_atomic_shared_allocations): "
+          << (dev.has(aspect::usm_atomic_shared_allocations)
+                  ? "Yes"
+                  : "No")
+          << "\n";
+      std::cout << "    has(usm_system_allocations): "
+                << (dev.has(aspect::usm_system_allocations)
+                        ? "Yes"
+                        : "No")
                 << "\n";
 
       do_query<info::device::vendor>(
@@ -61,3 +122,147 @@ int main() {
   }
   return 0;
 }
+
+#if 0
+// Some sample outputs:
+
+Found Platform:
+    info::platform::name is 'Intel(R) FPGA Emulation Platform for OpenCL(TM)'
+    info::platform::vendor is 'Intel(R) Corporation'
+    info::platform::version is 'OpenCL 1.2 Intel(R) FPGA SDK for OpenCL(TM), Version 20.3'
+    info::platform::profile is 'EMBEDDED_PROFILE'
+  Device: Intel(R) FPGA Emulation Device
+    is_cpu(): No
+    is_gpu(): No
+    is_accelerator(): Yes
+    has(fp16): No
+    has(fp64): Yes
+    has(atomic64): No
+    has(queue_profiling): Yes
+    has(usm_device_allocations): Yes
+    has(usm_host_allocations): Yes
+    has(usm_atomic_host_allocations): Yes
+    has(usm_shared_allocations): Yes
+    has(usm_atomic_shared_allocations): Yes
+    has(usm_system_allocations): No
+    info::device::vendor is 'Intel(R) Corporation'
+    info::device::driver_version is '2023.15.3.0.20_160000'
+    info::device::max_work_item_dimensions is '3'
+    info::device::max_work_group_size is '67108864'
+    info::device::mem_base_addr_align is '1024'
+    info::device::partition_max_sub_devices is '8'
+    Many more queries are available than shown here!
+
+Found Platform:
+    info::platform::name is 'Intel(R) OpenCL'
+    info::platform::vendor is 'Intel(R) Corporation'
+    info::platform::version is 'OpenCL 3.0 LINUX'
+    info::platform::profile is 'FULL_PROFILE'
+  Device: Intel(R) Core(TM) i7-8665U CPU @ 1.90GHz
+    is_cpu(): Yes
+    is_gpu(): No
+    is_accelerator(): No
+    has(fp16): No
+    has(fp64): Yes
+    has(atomic64): Yes
+    has(queue_profiling): Yes
+    has(usm_device_allocations): Yes
+    has(usm_host_allocations): Yes
+    has(usm_atomic_host_allocations): Yes
+    has(usm_shared_allocations): Yes
+    has(usm_atomic_shared_allocations): Yes
+    has(usm_system_allocations): Yes
+    info::device::vendor is 'Intel(R) Corporation'
+    info::device::driver_version is '2023.15.3.0.20_160000'
+    info::device::max_work_item_dimensions is '3'
+    info::device::max_work_group_size is '8192'
+    info::device::mem_base_addr_align is '1024'
+    info::device::partition_max_sub_devices is '8'
+    Many more queries are available than shown here!
+
+=====
+      
+Found Platform:
+    info::platform::name is 'Intel(R) OpenCL'
+    info::platform::vendor is 'Intel(R) Corporation'
+    info::platform::version is 'OpenCL 3.0 LINUX'
+    info::platform::profile is 'FULL_PROFILE'
+  Device: Intel(R) Xeon(R) E-2176G CPU @ 3.70GHz
+    is_cpu(): Yes
+    is_gpu(): No
+    is_accelerator(): No
+    has(fp16): No
+    has(fp64): Yes
+    has(atomic64): Yes
+    has(queue_profiling): Yes
+    has(usm_device_allocations): Yes
+    has(usm_host_allocations): Yes
+    has(usm_atomic_host_allocations): Yes
+    has(usm_shared_allocations): Yes
+    has(usm_atomic_shared_allocations): Yes
+    has(usm_system_allocations): Yes
+    info::device::vendor is 'Intel(R) Corporation'
+    info::device::driver_version is '2023.15.3.0.20_160000'
+    info::device::max_work_item_dimensions is '3'
+    info::device::max_work_group_size is '8192'
+    info::device::mem_base_addr_align is '1024'
+    info::device::partition_max_sub_devices is '12'
+    Many more queries are available than shown here!
+
+Found Platform:
+    info::platform::name is 'Intel(R) OpenCL HD Graphics'
+    info::platform::vendor is 'Intel(R) Corporation'
+    info::platform::version is 'OpenCL 3.0 '
+    info::platform::profile is 'FULL_PROFILE'
+  Device: Intel(R) UHD Graphics P630 [0x3e96]
+    is_cpu(): No
+    is_gpu(): Yes
+    is_accelerator(): No
+    has(fp16): Yes
+    has(fp64): Yes
+    has(atomic64): Yes
+    has(queue_profiling): Yes
+    has(usm_device_allocations): Yes
+    has(usm_host_allocations): Yes
+    has(usm_atomic_host_allocations): No
+    has(usm_shared_allocations): Yes
+    has(usm_atomic_shared_allocations): No
+    has(usm_system_allocations): No
+    info::device::vendor is 'Intel(R) Corporation'
+    info::device::driver_version is '22.35.24055'
+    info::device::max_work_item_dimensions is '3'
+    info::device::max_work_group_size is '256'
+    info::device::mem_base_addr_align is '1024'
+    info::device::partition_max_sub_devices is '0'
+    Many more queries are available than shown here!
+
+Found Platform:
+    info::platform::name is 'Intel(R) Level-Zero'
+    info::platform::vendor is 'Intel(R) Corporation'
+    info::platform::version is '1.3'
+    info::platform::profile is 'FULL_PROFILE'
+  Device: Intel(R) UHD Graphics P630 [0x3e96]
+    is_cpu(): No
+    is_gpu(): Yes
+    is_accelerator(): No
+    has(fp16): Yes
+    has(fp64): Yes
+    has(atomic64): Yes
+    has(queue_profiling): Yes
+    has(usm_device_allocations): Yes
+    has(usm_host_allocations): Yes
+    has(usm_atomic_host_allocations): No
+    has(usm_shared_allocations): Yes
+    has(usm_atomic_shared_allocations): No
+    has(usm_system_allocations): No
+    info::device::vendor is 'Intel(R) Corporation'
+    info::device::driver_version is '1.3.24055'
+    info::device::max_work_item_dimensions is '3'
+    info::device::max_work_group_size is '256'
+    info::device::mem_base_addr_align is '8'
+    info::device::partition_max_sub_devices is '0'
+    Many more queries are available than shown here!
+
+
+      
+#endif
