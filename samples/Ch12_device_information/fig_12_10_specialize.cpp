46de049ead31f3f5a650a0c48316913583159628
James Reinders <james.r.reinders@intel.com>
2023-05-08 14:23:29 -0700

    fixed figure 10 - was overwritten by mistake?  fixed now

--- samples/Ch12_device_information/fig_12_10_specialize.cpp
+++ samples/Ch12_device_information/fig_12_10_specialize.cpp
@@ -1,63 +1,52 @@
-// Copyright (C) 2023 Intel Corporation
-
-// SPDX-License-Identifier: MIT
-
 #include <iostream>
 #include <sycl/sycl.hpp>
 using namespace sycl;
 
-template <typename queryT, typename T>
-void do_query(const T& obj_to_query,
-              const std::string& name, int indent = 4) {
-  std::cout << std::string(indent, ' ') << name << " is '"
-            << obj_to_query.template get_info<queryT>()
-            << "'\n";
-}
-
 int main() {
-  // Loop through the available platforms
-  for (auto const& this_platform :
-       platform::get_platforms()) {
-    std::cout << "Found Platform:\n";
-    do_query<info::platform::name>(this_platform,
-                                   "info::platform::name");
-    do_query<info::platform::vendor>(
-        this_platform, "info::platform::vendor");
-    do_query<info::platform::version>(
-        this_platform, "info::platform::version");
-    do_query<info::platform::profile>(
-        this_platform, "info::platform::profile");
+  queue MyQ;
 
-    // Loop through the devices available in this plaform
-    for (auto& dev : this_platform.get_devices()) {
-      std::cout << "  Device: "
-                << dev.get_info<info::device::name>()
-                << "\n";
-      std::cout << "    is_cpu(): "
-                << (dev.is_cpu() ? "Yes" : "No") << "\n";
-      std::cout << "    is_gpu(): "
-                << (dev.is_gpu() ? "Yes" : "No") << "\n";
-      std::cout << "    is_accelerator(): "
-                << (dev.is_accelerator() ? "Yes" : "No")
-                << "\n";
+  constexpr int size = 16;
+  std::array<double, size> data;
 
-      do_query<info::device::vendor>(
-          dev, "info::device::vendor");
-      do_query<info::device::driver_version>(
-          dev, "info::device::driver_version");
-      do_query<info::device::max_work_item_dimensions>(
-          dev, "info::device::max_work_item_dimensions");
-      do_query<info::device::max_work_group_size>(
-          dev, "info::device::max_work_group_size");
-      do_query<info::device::mem_base_addr_align>(
-          dev, "info::device::mem_base_addr_align");
-      do_query<info::device::partition_max_sub_devices>(
-          dev, "info::device::partition_max_sub_devices");
-
-      std::cout << "    Many more queries are available "
-                   "than shown here!\n";
+  // Using "sycl::device_has()" as an attribute does not
+  // affect the device we select. Therefore, our host code
+  // should check the device's aspects before submitting a
+  // kernel which does require that attribute.
+  if (MyQ.get_device().has(aspect::fp64)) {
+    buffer B{data};
+    MyQ.submit([&](handler& h) {
+      accessor A{B, h};
+      // the attributes here say that the kernel is allowed
+      // to require fp64 support any attribute(s) from
+      // Figure 12-3 could be specified note that namespace
+      // stmt above (for C++) does not affect attributes (a
+      // C++ quirk) so sycl:: is needed here
+      h.parallel_for(
+          size, [=](auto& idx)
+                    [[sycl::device_has(aspect::fp64)]] {
+                      A[idx] = idx * 2.0;
+                    });
+    });
+    std::cout << "doubles were used\n";
+  } else {
+    // here we use an alternate method (not needing double
+    // math support on the device) to help our code be
+    // flexible and hence more portable
+    std::array<float, size> fdata;
+    {
+      buffer B{fdata};
+      MyQ.submit([&](handler& h) {
+        accessor A{B, h};
+        h.parallel_for(
+            size, [=](auto& idx) { A[idx] = idx * 2.0f; });
+      });
     }
-    std::cout << "\n";
+
+    for (int i = 0; i < size; i++) data[i] = fdata[i];
+
+    std::cout << "no doubles used\n";
   }
+  for (int i = 0; i < size; i++)
+    std::cout << "data[" << i << "] = " << data[i] << "\n";
   return 0;
 }
