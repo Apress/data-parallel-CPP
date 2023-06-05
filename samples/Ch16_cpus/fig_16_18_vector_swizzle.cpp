61c4bb870af4619e1334f27b3ed7ed534d96b3ff
Michael Kinsner <michael.kinsner@intel.com>
2023-06-05 10:36:01 -0700

    Don't use octal initializer

--- samples/Ch16_cpus/fig_16_18_vector_swizzle.cpp
+++ samples/Ch16_cpus/fig_16_18_vector_swizzle.cpp
@@ -16,11 +16,11 @@ int main() {
 
   q.single_task([=]() {
      sycl::vec<int, 4> old_v =
-         sycl::vec<int, 4>(000, 100, 200, 300);
+         sycl::vec<int, 4>(0, 100, 200, 300);
      sycl::vec<int, 4> new_v = sycl::vec<int, 4>();
 
      new_v.rgba() = old_v.abgr();
-     int vals[] = {300, 200, 100, 000};
+     int vals[] = {300, 200, 100, 0};
 
      if (new_v.r() != vals[0] || new_v.g() != vals[1] ||
          new_v.b() != vals[2] || new_v.a() != vals[3]) {
