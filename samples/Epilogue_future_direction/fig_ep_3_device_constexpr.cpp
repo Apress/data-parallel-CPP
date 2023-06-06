f582835797cec1e7f769bcfdd282991af5dc708f
John Pennycook <john.pennycook@intel.com>
2023-06-06 08:58:06 -0700

    Use if_device_has for devconstexpr example
    This reflects our latest thinking and the design of the
    sycl_ext_oneapi_device_if extension.

--- samples/Epilogue_future_direction/fig_ep_3_device_constexpr.cpp
+++ samples/Epilogue_future_direction/fig_ep_3_device_constexpr.cpp
@@ -12,14 +12,13 @@ int main() {
   q.submit([&](handler& h) {
      stream out(9, 9, h);
      h.parallel_for(range{1}, [=](id<1> idx) {
-       if devconstexpr (this_device().has<aspect::cpu>()) {
+       if_device_has<aspect::cpu>([&]() {
          /* Code specialized for CPUs */
          out << "On a CPU!" << endl;
-       } else if devconstexpr (this_device()
-                                   .has<aspect::gpu>()) {
+       }).else_if_device_has<aspect::gpu>([&]() {
          /* Code specialized for GPUs */
          out << "On a GPU!" << endl;
-       }
+       });
      });
    }).wait();
 }
