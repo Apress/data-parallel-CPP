5608a9adc1e31a048b0ab9a0b2aae55d9c4c6b85
James Reinders <james.r.reinders@intel.com>
2023-06-06 20:26:56 -0700

    add missing hpp to capture code not previously logged in a file

--- samples/Ch07_buffers/fig_7_1.hpp
+++ samples/Ch07_buffers/fig_7_1.hpp
@@ -1,2 +1,9 @@
+// Copyright (C) 2023 Intel Corporation
+
+// SPDX-License-Identifier: MIT
+
+// These ".hpp" files are text from the book that are
+// snippets that are not set up to be compiled as is.
+
 template <typename T, int Dimensions, AllocatorT allocator>
 class buffer;
