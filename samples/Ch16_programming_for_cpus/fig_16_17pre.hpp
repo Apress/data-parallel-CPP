1474452bb0fd0c16b010dded74c29f9e5ed0b9e9
James Reinders <james.r.reinders@intel.com>
2023-06-12 17:19:08 -0700

    fixed typo here and in 2nd edition draft

--- samples/Ch16_programming_for_cpus/fig_16_17pre.hpp
+++ samples/Ch16_programming_for_cpus/fig_16_17pre.hpp
@@ -5,4 +5,4 @@
 // These ".hpp" files are text from the book that are
 // snippets that are not set up to be compiled as is.
 
-int id = get global id(0); a[id] = b[id] + c[id];
+int id = get_global_id(0); a[id] = b[id] + c[id];
