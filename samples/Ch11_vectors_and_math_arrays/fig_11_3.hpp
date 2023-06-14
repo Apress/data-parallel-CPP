b8a0efbca86260301af173096f9527b79bf943ad
John Pennycook <john.pennycook@intel.com>
2023-06-14 13:26:38 -0700

    Fix typo in load/store interfaces (#114)
    "multi_ptr ptr" should just be "multi_ptr".

--- samples/Ch11_vectors_and_math_arrays/fig_11_3.hpp
+++ samples/Ch11_vectors_and_math_arrays/fig_11_3.hpp
@@ -6,7 +6,7 @@
 // snippets that are not set up to be compiled as is.
 
 template <access::address_space AddressSpace, access::decorated IsDecorated> 
-  void load(size_t offset, multi_ptr ptr<DataT, AddressSpace, IsDecorated> ptr);
+  void load(size_t offset, multi_ptr<DataT, AddressSpace, IsDecorated> ptr);
 
 template <access::address_space addressSpace, access::decorated IsDecorated>
-  void store(size_t offset, multi_ptr ptr<DataT, AddressSpace, IsDecorated> ptr) const;
+  void store(size_t offset, multi_ptr<DataT, AddressSpace, IsDecorated> ptr) const;
