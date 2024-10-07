63408c1b9495ac8db6aca8cba3177140f0619c75
Nuno Nobre <nuno.nobre@stfc.ac.uk>
2024-10-07 15:52:29 +0100

    Address minor issues in Figures 2-19 and 3-17 (#138)
    * Fix return types for memset/memcpy in Figure 2-19 (#138)
    
    * Fix parameter name and indentation in Figure 3-17 (#138)
    
    * Added errata entries for Figures 2-19 and 3-17 (#138)

--- samples/Ch03_data_management/fig_3_17.hpp
+++ samples/Ch03_data_management/fig_3_17.hpp
@@ -7,9 +7,9 @@
 
 class handler {
   ...
-      // Specifies event(s) that must be complete before the
-      // action defined in this command group executes.
-      void depends_on({event / std::vector<event> & });
+  // Specifies event(s) that must be complete before the
+  // action defined in this command group executes.
+  void depends_on({event / std::vector<event> & });
 
   // Enqueues a memcpy from Src to Dest.
   // Count bytes are copied.
@@ -24,20 +24,20 @@ class handler {
   // Writes the first byte of Value into Count bytes.
   void memset(void* Ptr, int Value, size_t Count)
 
-      // Enques a fill operation on the specified pointer.
-      // Fills Pattern into Ptr Count times.
-      template <typename T>
-      void fill(void* Ptr, const T& Pattern, size_t Count);
+  // Enques a fill operation on the specified pointer.
+  // Fills Pattern into Ptr Count times.
+  template <typename T>
+  void fill(void* Ptr, const T& Pattern, size_t Count);
 
   // Submits a kernel of one work-item for execution.
   template <typename KernelName, typename KernelType>
   void single_task(KernelType KernelFunc);
 
-  // Submits a kernel with NumWork-items work-items for
+  // Submits a kernel with NumWorkItems work-items for
   // execution.
   template <typename KernelName, typename KernelType,
             int Dims>
-  void parallel_for(range<Dims> NumWork - items,
+  void parallel_for(range<Dims> NumWorkItems,
                     KernelType KernelFunc);
 
   // Submits a kernel for execution over the supplied
