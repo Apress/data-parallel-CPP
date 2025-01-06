42474acfe63bc95acf6deafd7f8e67cbf9962d7c
Ben Ashbaugh <ben.ashbaugh@intel.com>
2025-01-06 08:42:16 -0800

    handle missing device types in fig_2_12_multiple_selectors (#140)

--- samples/Ch02_where_code_runs/fig_2_12_multiple_selectors.cpp
+++ samples/Ch02_where_code_runs/fig_2_12_multiple_selectors.cpp
@@ -17,14 +17,34 @@ void output_dev_info(const device& dev,
 }
 
 int main() {
-  output_dev_info(device{default_selector_v},
-                  "default_selector_v");
-  output_dev_info(device{cpu_selector_v}, "cpu_selector_v");
-  output_dev_info(device{gpu_selector_v}, "gpu_selector_v");
-  output_dev_info(device{accelerator_selector_v},
-                  "accelerator_selector_v");
-  output_dev_info(device{ext::intel::fpga_selector_v},
-                  "fpga_selector_v");
+  try {
+    output_dev_info(device{default_selector_v},
+                    "default_selector_v");
+  } catch (sycl::exception& e) {
+    std::cout << "No SYCL devices found.\n";
+  }
+  try {
+    output_dev_info(device{cpu_selector_v}, "cpu_selector_v");
+  } catch (sycl::exception& e) {
+    std::cout << "No CPU devices found.\n";
+  }
+  try {
+    output_dev_info(device{gpu_selector_v}, "gpu_selector_v");
+  } catch (sycl::exception& e) {
+    std::cout << "No GPU devices found.\n";
+  }
+  try {
+    output_dev_info(device{accelerator_selector_v},
+                    "accelerator_selector_v");
+  } catch (sycl::exception& e) {
+    std::cout << "No accelerator devices found.\n";
+  }
+  try {
+    output_dev_info(device{ext::intel::fpga_selector_v},
+                    "fpga_selector_v");
+  } catch (sycl::exception& e) {
+    std::cout << "No FPGA devices found.\n";
+  }
 
   return 0;
 }
@@ -32,11 +52,11 @@ int main() {
 // Example Output:
 // default_selector_v: Selected device: Intel(R) UHD Graphics [0x9a60]
 //   -> Device vendor: Intel(R) Corporation
-//   cpu_selector_v: Selected device: 11th Gen Intel(R) Core(TM) i9-11900KB @ 3.30GHz
+// cpu_selector_v: Selected device: 11th Gen Intel(R) Core(TM) i9-11900KB @ 3.30GHz
 //   -> Device vendor: Intel(R) Corporation
-//   gpu_selector_v: Selected device: Intel(R) UHD Graphics [0x9a60]
+// gpu_selector_v: Selected device: Intel(R) UHD Graphics [0x9a60]
 //   -> Device vendor: Intel(R) Corporation
-//   accelerator_selector_v: Selected device: Intel(R) FPGA Emulation Device
+// accelerator_selector_v: Selected device: Intel(R) FPGA Emulation Device
 //   -> Device vendor: Intel(R) Corporation
-//   fpga_selector_v: Selected device: pac_a10 : Intel PAC Platform (pac_ee00000)
-// -> Device vendor: Intel Corp
+// fpga_selector_v: Selected device: pac_a10 : Intel PAC Platform (pac_ee00000)
+//   -> Device vendor: Intel Corp
